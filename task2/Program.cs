5ae6fe6dd0f1aa98cba9430254a546a9c0f9e72e
NazrinMammadova <102550859+NazrinMammadova@users.noreply.github.com>
2022-04-14 03:58:56 -0700

    added file

---
+++ task2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            int cem = 0;
+            int say = 234;
+
+            while (say > 0)
+            {
+
+                int reqem = say % 10;
+                cem = cem + reqem;
+                say = (say - reqem) / 10;
+
+            }
+
+
+            Console.WriteLine(cem);
+
+
+
+
+
+
+
+        }
+    }
+}
